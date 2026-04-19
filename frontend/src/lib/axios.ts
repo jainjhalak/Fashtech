d7f63c95db31a1820911dfd9be7ee697058519a7
jainjhalak <jainjhalak9491@gmail.com>
2026-04-19 18:58:13 +0530

    create board optimized

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL
-    ? `${process.env.NEXT_PUBLIC_API_URL}/api`
+    ? `${process.env.NEXT_PUBLIC_API_URL}`
     : "http://localhost:3000/api",
 });
 
